7decadf5ab23ecce1d941c7afa08921d0674a10e
Daniel Sias <daniel.sias@gmail.com>
2019-08-07 00:05:30 -0400

    Program belongs to a Partner

--- app/Program.php
+++ app/Program.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Program extends Model
 {
     public $timestamps = false;
+
+    public function partner()
+    {
+        $this->belongsTo(Partner::class);
+    }
 }
