7ede93482d7e3595e70a70dd41ca57aaa271e469
Daniel Sias <daniel.sias@gmail.com>
2019-08-06 22:58:28 -0400

    Added Google OAuth values

--- config/services.php
+++ config/services.php
@@ -13,6 +13,12 @@ return [
     | to have a conventional place to find your various credentials.
     |
     */
+    
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_CALLBACK_URL'),
+    ],
 
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
