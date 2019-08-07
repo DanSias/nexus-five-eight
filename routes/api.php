417fd89092566dc5627ba921716def0d8323cb90
Daniel Sias <daniel.sias@gmail.com>
2019-08-07 00:06:04 -0400

    Added notes routes

--- routes/api.php
+++ routes/api.php
@@ -2,20 +2,18 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->group(function () {
+
+    // Users
     Route::post('/users', 'UserController@store');
     Route::get('/users/{user}', 'UserController@show');
     Route::patch('/users/{user}', 'UserController@update');
     Route::delete('/users/{user}', 'UserController@destroy');
+
+    // Notes
+    Route::post('/notes', 'NoteController@store');
+    Route::get('/notes/{user}', 'NoteController@show');
+    Route::patch('/notes/{user}', 'NoteController@update');
+    Route::delete('/notes/{user}', 'NoteController@destroy');
+    
 });
