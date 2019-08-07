5c5af74ee7b59596fcf9a5b80e5bc9b093646c0f
Daniel Sias <daniel.sias@gmail.com>
2019-08-06 22:41:30 -0400

    Program requirements and admissions details

---
+++ database/factories/ProgramRequirementFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ProgramRequirement;
+use Faker\Generator as Faker;
+
+$factory->define(ProgramRequirement::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
