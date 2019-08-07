5ac28c88168a1ceaebbe506c8c3f9a9535484c0b
Daniel Sias <daniel.sias@gmail.com>
2019-08-06 22:41:46 -0400

    User programs owned

---
+++ database/factories/UserProgramFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\UserProgram;
+use Faker\Generator as Faker;
+
+$factory->define(UserProgram::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
