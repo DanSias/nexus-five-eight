0578405af7ce74cdb51b60e014d786e23e623149
Daniel Sias <daniel.sias@gmail.com>
2019-08-09 22:42:03 -0400

    Recruitment Budget

---
+++ database/factories/RecruitmentBudgetFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\RecruitmentBudget;
+use Faker\Generator as Faker;
+
+$factory->define(RecruitmentBudget::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
