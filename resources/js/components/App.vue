9a0ada50a40f1e30ffe02ee1d8c54b7d2058a03a
Daniel Sias <daniel.sias@gmail.com>
2019-09-03 15:28:32 -0400

    Base component

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <h2>Hello from App</h2>
+        <nexus-nav></nexus-nav>
+        <div class="p-y-8 p-x-6">
+            <h2 class="bg-blue-500">Hello from App</h2>
+        </div>
 
         <router-view></router-view>
     </div>
@@ -8,7 +11,10 @@
 
 <script>
     export default {
-        name: 'nexus'
+        name: 'nexus',
+        components: {
+            NexusNav: () => import('./Nav')
+        }
     }
 </script>
 
